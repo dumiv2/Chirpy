df89ee496186d596f1bd89b3b855dd997b39f9c0
Hardcorelevelingwarrior <nhuducminh123@gmail.com>
2023-08-04 15:10:01 +0700

    finsh json

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strings"
+
 	"github.com/go-chi/chi/v5"
 	_ "github.com/go-chi/chi/v5/middleware"
 )
@@ -17,16 +21,68 @@ func main() {
 	r.Handle("/app/*",fsHandler)
 	r.Handle("/app",fsHandler)
 
-	r.Get("/healthz", func(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("OK"))
+	c := chi.NewRouter()
+	d := chi.NewRouter()
+	c.Get("/healthz", func(w http.ResponseWriter, r *http.Request){
 		w.WriteHeader(200)
 		w.Header().Add("Content-Type","text/plain; charset=utf-8")
+		w.Write([]byte("OK"))
 	})
-	r.Get("/metrics", func (w http.ResponseWriter , r *http.Request){
+	c.Post("/validate_chirp",func(w http.ResponseWriter, r *http.Request) {
+		type chirp struct {
+			Body string `json:"body"`
+		}
+		type validate struct {
+			Err string `json:"error,omitempty"`
+			Valid bool `json:"valid,omitempty"`
+			Cleanbody string `json:"cleaned_body,omitempty"`
+		}
+		validate_chirp := validate{
+			Valid : true,
+		}
+		dat , _:= json.Marshal(validate_chirp)
+		decoder := json.NewDecoder(r.Body)
+		chirpee := chirp{}
+		err := decoder.Decode(&chirpee)
+		
+		if err != nil {
+			validate_chirp := validate{
+				Err : "Cannot marshaling" ,
+			}
+			dat,_ := json.Marshal(validate_chirp)
+			w.WriteHeader(400)
+			w.Write(dat)
+			return
+		}
+		if len(chirpee.Body) > 140 {
+			validate_chirp := validate{
+				Err : "Chirp is too long" ,
+			}
+			dat,_ := json.Marshal(validate_chirp)
+			w.WriteHeader(400)
+			w.Write(dat)
+			return
+		}
+		w.Write(dat)
+	})
+	d.Get("/metrics", func (w http.ResponseWriter , r *http.Request){
+		w.Header().Add("Content-Type","text/html")
 
 		w.Write([]byte(fmt.Sprintf("Hits : %v",cfg.fileserverHits) ))
 	})
+	r.Mount("/api",c)
+	r.Mount("/admin",d)
 
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data, err := ioutil.ReadFile("index.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		content := strings.ReplaceAll(string(data), "%d", fmt.Sprintf("%d", cfg.fileserverHits))
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.Write([]byte(content))
+	})
 	corsMux := middlewareCors(r)
 
 	srv := &http.Server {
